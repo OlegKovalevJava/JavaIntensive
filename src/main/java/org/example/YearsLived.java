2ff113dd507cb5e6ee19d1336b37fc5dda1ce714
Oleg <kovalyovop@yandex.ru>
2023-11-14 17:12:19 +0300

    Training task for warming up 14.11.2023

---
+++ src/main/java/org/example/YearsLived.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class YearsLived {
+    public void printAge() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите число от 1 до 120 включительно:");
+
+        int year = scanner.nextInt();
+
+        scanner.close();
+
+        if (year >= 11 && year <= 14) {
+            System.out.println("Вам " + year + " лет");
+        } else if (year >= 111 && year <= 114) {
+            System.out.println("Вам " + year + " лет");
+        } else if (year % 10 == 1) {
+            System.out.println("Вам " + year + " год");
+        } else if (year % 10 == 2 || year % 10 == 3 || year % 10 == 4) {
+            System.out.println("Вам " + year + " года");
+        } else {
+            System.out.println("Вам " + year + " лет");
+        }
+    }
+}
